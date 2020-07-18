13b9cb15609cfc6c0206bb53c80eee3a32f26f89
willianlemes <willianlemes94@gmail.com>
2020-07-17 21:19:25 -0300

    Configuração do multer

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'world' });
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+  return res.json({ hello: 'Rocketseat' });
 });
 
 module.exports = routes;
